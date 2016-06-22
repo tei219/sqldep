4f2e2b9f10a57ae2fc0df3749990e4ad1cc1f30f
tei219 <tei219@gmail.com>
2016-06-22 10:15:27 +0900

    Update tsqldep.ps1
    remove reserved_keywords
    implement -version option

--- tsqldep.ps1
+++ tsqldep.ps1
@@ -88,7 +88,7 @@ IfStatements のなかのCRUD
 [CmdletBinding()]
 #to operate option order: -p > -f > -t > -c > -r > parse > format > tables > crud > tree
 param (
-	[parameter(Mandatory=$true, helpmessage="string/file/directory")] [alias('s')] [string] $source = $args[0],
+	[parameter(Mandatory=$true, helpmessage="input ""tsql-string"" or file/directory to parse.")] [alias('s')] [string] $source = $args[0],
 	[parameter()] [ValidateSet( 'crud','tables','parse','tree','format' )] [string] $op = "parse",
 	[switch] $c = $false,
 	[switch] $t = $false,
@@ -96,8 +96,7 @@ param (
 	[switch] $r = $false,
 	[switch] $f = $false,
 	[switch] $desc = $false,
-	[parameter()] $version,
-	[parameter()] $sizelimit
+	[parameter()] [ValidateSet( 'TSql80Parser','TSql90Parser','TSql100Parser','TSql110Parser','TSql120Parser' )] [string] $version = "TSql110Parser"
 )
 
 if ($r) { $op = "tree" }
@@ -106,80 +105,7 @@ if ($t) { $op = "tables" }
 if ($f) { $op = "format" }
 if ($p) { $op = "parse" }
 
-$reserved_keyword = @("ADD","ALL","ALTER","AND","ANY","AS","ASC","AUTHORIZATION","BACKUP","BEGIN",
-"BETWEEN","BREAK","BROWSE","BULK","BY","CASCADE","CASE","CHECK","CHECKPOINT","CLOSE","CLUSTERED",
-"COALESCE","COLLATE","COLUMN","COMMIT","COMPUTE","CONSTRAINT","CONTAINS","CONTAINSTABLE",
-"CONTINUE","CONVERT","CREATE","CROSS","CURRENT","CURRENT_DATE","CURRENT_TIME","CURRENT_TIMESTAMP",
-"CURRENT_USER","CURSOR","DATABASE","DBCC","DEALLOCATE","DECLARE","DEFAULT","DELETE","DENY","DESC",
-"DISK","DISTINCT","DISTRIBUTED","DOUBLE","DROP","DUMP","ELSE","END","ERRLVL","ESCAPE","EXCEPT",
-"EXEC","EXECUTE","EXISTS","EXIT","EXTERNAL","FETCH","FILE","FILLFACTOR","FOR","FOREIGN","FREETEXT",
-"FREETEXTTABLE","FROM","FULL","FUNCTION","GOTO","GRANT","GROUP","HAVING","HOLDLOCK","IDENTITY",
-"IDENTITY_INSERT","IDENTITYCOL","IF","IN","INDEX","INNER","INSERT","INTERSECT","INTO","IS","JOIN",
-"KEY","KILL","LEFT","LIKE","LINENO","LOAD","MERGE","NATIONAL","NOCHECK","NONCLUSTERED","NOT","NULL",
-"NULLIF","OF","OFF","OFFSETS","ON","OPEN","OPENDATASOURCE","OPENQUERY","OPENROWSET","OPENXML",
-"OPTION","OR","ORDER","OUTER","OVER","PERCENT","PIVOT","PLAN","PRECISION","PRIMARY","PRINT","PROC",
-"PROCEDURE","PUBLIC","RAISERROR","READ","READTEXT","RECONFIGURE","REFERENCES","REPLICATION","RESTORE",
-"RESTRICT","RETURN","REVERT","REVOKE","RIGHT","ROLLBACK","ROWCOUNT","ROWGUIDCOL","RULE","SAVE","SCHEMA",
-"SECURITYAUDIT","SELECT","SEMANTICKEYPHRASETABLE","SEMANTICSIMILARITYDETAILSTABLE","SEMANTICSIMILARITYTABLE",
-"SESSION_USER","SET","SETUSER","SHUTDOWN","SOME","STATISTICS","SYSTEM_USER","TABLE","TABLESAMPLE",
-"TEXTSIZE","THEN","TO","TOP","TRAN","TRANSACTION","TRIGGER","TRUNCATE","TRY_CONVERT","TSEQUAL","UNION",
-"UNIQUE","UNPIVOT","UPDATE","UPDATETEXT","USE","USER","VALUES","VARYING","VIEW","WAITFOR","WHEN",
-"WHERE","WHILE","WITH","WITHIN GROUP","WRITETEXT")
-
-$reserved_keyword_odbc = @("ABSOLUTE","ACTION","ADA","ADD","ALL","ALLOCATE","ALTER","AND","ANY","ARE",
-"AS","ASC","ASSERTION","AT","AUTHORIZATION","AVG","BEGIN","BETWEEN","BIT","BIT_LENGTH","BOTH","BY",
-"CASCADE","CASCADED","CASE","CAST","CATALOG","CHAR","CHAR_LENGTH","CHARACTER","CHARACTER_LENGTH",
-"CHECK","CLOSE","COALESCE","COLLATE","COLLATION","COLUMN","COMMIT","CONNECT","CONNECTION","CONSTRAINT",
-"CONSTRAINTS","CONTINUE","CONVERT","CORRESPONDING","COUNT","CREATE","CROSS","CURRENT","CURRENT_DATE",
-"CURRENT_TIME","CURRENT_TIMESTAMP","CURRENT_USER","CURSOR","DATE","DAY","DEALLOCATE","DEC","DECIMAL",
-"DECLARE","DEFAULT","DEFERRABLE","DEFERRED","DELETE","DESC","DESCRIBE","DESCRIPTOR","DIAGNOSTICS",
-"DISCONNECT","DISTINCT","DOMAIN","DOUBLE","DROP","ELSE","END","END-EXEC","ESCAPE","EXCEPT","EXCEPTION",
-"EXEC","EXECUTE","EXISTS","EXTERNAL","EXTRACT","FALSE","FETCH","FIRST","FLOAT","FOR","FOREIGN","FORTRAN",
-"FOUND","FROM","FULL","GET","GLOBAL","GO","GOTO","GRANT","GROUP","HAVING","HOUR","IDENTITY","IMMEDIATE",
-"IN","INCLUDE","INDEX","INDICATOR","INITIALLY","INNER","INPUT","INSENSITIVE","INSERT","INT","INTEGER",
-"INTERSECT","INTERVAL","INTO","IS","ISOLATION","JOIN","KEY","LANGUAGE","LAST","LEADING","LEFT","LEVEL",
-"LIKE","LOCAL","LOWER","MATCH","MAX","MIN","MINUTE","MODULE","MONTH","NAMES","NATIONAL","NATURAL","NCHAR",
-"NEXT","NO","NONE","NOT","NULL","NULLIF","NUMERIC","OCTET_LENGTH","OF","ON","ONLY","OPEN","OPTION","OR",
-"ORDER","OUTER","OUTPUT","OVERLAPS","PAD","PARTIAL","PASCAL","POSITION","PRECISION","PREPARE","PRESERVE",
-"PRIMARY","PRIOR","PRIVILEGES","PROCEDURE","PUBLIC","READ","REAL","REFERENCES","RELATIVE","RESTRICT",
-"REVOKE","RIGHT","ROLLBACK","ROWS","SCHEMA","SCROLL","SECOND","SECTION","SELECT","SESSION","SESSION_USER",
-"SET","SIZE","SMALLINT","SOME","SPACE","SQL","SQLCA","SQLCODE","SQLERROR","SQLSTATE","SQLWARNING","SUBSTRING",
-"SUM","SYSTEM_USER","TABLE","TEMPORARY","THEN","TIME","TIMESTAMP","TIMEZONE_HOUR","TIMEZONE_MINUTE","TO",
-"TRAILING","TRANSACTION","TRANSLATE","TRANSLATION","TRIM","TRUE","UNION","UNIQUE","UNKNOWN","UPDATE","UPPER",
-"USAGE","USER","USING","VALUE","VALUES","VARCHAR","VARYING","VIEW","WHEN","WHENEVER","WHERE","WITH","WORK",
-"WRITE","YEAR","ZONE")
-
-$reserved_keyword_feature = @("ABSOLUTE","ACTION","ADMIN","AFTER","AGGREGATE","ALIAS","ALLOCATE","ARE",
-"ARRAY","ASENSITIVE","ASSERTION","ASYMMETRIC","AT","ATOMIC","BEFORE","BINARY","BIT","BLOB","BOOLEAN",
-"BOTH","BREADTH","CALL","CALLED","CARDINALITY","CASCADED","CAST","CATALOG","CHAR","CHARACTER","CLASS",
-"CLOB","COLLATION","COLLECT","COMPLETION","CONDITION","CONNECT","CONNECTION","CONSTRAINTS","CONSTRUCTOR",
-"CORR","CORRESPONDING","COVAR_POP","COVAR_SAMP","CUBE","CUME_DIST","CURRENT_CATALOG","CURRENT_DEFAULT_TRANSFORM_GROUP",
-"CURRENT_PATH","CURRENT_ROLE","CURRENT_SCHEMA","CURRENT_TRANSFORM_GROUP_FOR_TYPE","CYCLE","DATA","DATE","DAY",
-"DEC","DECIMAL","DEFERRABLE","DEFERRED","DEPTH","DEREF","DESCRIBE","DESCRIPTOR","DESTROY","DESTRUCTOR","DETERMINISTIC",
-"DIAGNOSTICS","DICTIONARY","DISCONNECT","DOMAIN","DYNAMIC","EACH","ELEMENT","END-EXEC","EQUALS","EVERY","EXCEPTION",
-"FALSE","FILTER","FIRST","FLOAT","FOUND","FREE","FULLTEXTTABLE","FUSION","GENERAL","GET","GLOBAL","GO","GROUPING",
-"HOLD","HOST","HOUR","IGNORE","IMMEDIATE","INDICATOR","INITIALIZE","INITIALLY","INOUT","INPUT","INT","INTEGER",
-"INTERSECTION","INTERVAL","ISOLATION","ITERATE","LANGUAGE","LARGE","LAST","LATERAL","LEADING","LESS","LEVEL","LIKE_REGEX",
-"LIMIT","LN","LOCAL","LOCALTIME","LOCALTIMESTAMP","LOCATOR","MAP","MATCH","MEMBER","METHOD","MINUTE","MOD","MODIFIES",
-"MODIFY","MODULE","MONTH","MULTISET","NAMES","NATURAL","NCHAR","NCLOB","NEW","NEXT","NO","NONE","NORMALIZE","NUMERIC",
-"OBJECT","OCCURRENCES_REGEX","OLD","ONLY","OPERATION","ORDINALITY","OUT","OUTPUT","OVERLAY","PAD","PARAMETER","PARAMETERS",
-"PARTIAL","PARTITION","PATH","PERCENT_RANK","PERCENTILE_CONT","PERCENTILE_DISC","POSITION_REGEX","POSTFIX","PREFIX",
-"PREORDER","PREPARE","PRESERVE","PRIOR","PRIVILEGES","RANGE","READS","REAL","RECURSIVE","REF","REFERENCING","REGR_AVGX",
-"REGR_AVGY","REGR_COUNT","REGR_INTERCEPT","REGR_R2","REGR_SLOPE","REGR_SXX","REGR_SXY","REGR_SYY","RELATIVE",
-"RELEASE","RESULT","RETURNS","ROLE","ROLLUP","ROUTINE","ROW","ROWS","SAVEPOINT","SCOPE","SCROLL","SEARCH","SECOND",
-"SECTION","SENSITIVE","SEQUENCE","SESSION","SETS","SIMILAR","SIZE","SMALLINT","SPACE","SPECIFIC","SPECIFICTYPE","SQL",
-"SQLEXCEPTION","SQLSTATE","SQLWARNING","START","STATE","STATEMENT","STATIC","STDDEV_POP","STDDEV_SAMP","STRUCTURE",
-"SUBMULTISET","SUBSTRING_REGEX","SYMMETRIC","SYSTEM","TEMPORARY","TERMINATE","THAN","TIME","TIMESTAMP","TIMEZONE_HOUR",
-"TIMEZONE_MINUTE","TRAILING","TRANSLATE_REGEX","TRANSLATION","TREAT","TRUE","UESCAPE","UNDER","UNKNOWN","UNNEST","USAGE",
-"USING","VALUE","VAR_POP","VAR_SAMP","VARCHAR","VARIABLE","WHENEVER","WIDTH_BUCKET","WINDOW","WITHIN","WITHOUT","WORK",
-"WRITE","XMLAGG","XMLATTRIBUTES","XMLBINARY","XMLCAST","XMLCOMMENT","XMLCONCAT","XMLDOCUMENT","XMLELEMENT","XMLEXISTS",
-"XMLFOREST","XMLITERATE","XMLNAMESPACES","XMLPARSE","XMLPI","XMLQUERY","XMLSERIALIZE","XMLTABLE","XMLTEXT","XMLVALIDATE",
-"YEAR","ZONE")
-
-$reserved_keywords = ($reserved_keyword, $reserved_keyword_odbc, $reserved_keyword_feature)
-
 # import ######################################################################
-
 
 # workflows ###################################################################
 
@@ -288,16 +214,16 @@ function showquery {
 }
 
 # main ########################################################################
-# table format
-$tf_parse  = @{Expression={$_.type};Label="type";width=8}, @{Expression={$_.source};Label="source";width=40}, @{Expression={$_.parse_error};Label="parse_error";width=60}, @{Expression={$_.fragment};Label="fragment";width=60}
-$tf_tree   = @{Expression={$_.type};Label="type";width=8}, @{Expression={$_.source};Label="source";width=40}, @{Expression={$_.tree};Label="tree";width=60}, @{Expression={$_.query};Label="query";width=60}
-$tf_tables = @{Expression={$_.type};Label="type";width=8}, @{Expression={$_.source};Label="source";width=40}, @{Expression={$_.tables};Label="tables";width=60}, @{Expression={$_.query};Label="query";width=60}
-$tf_crud   = @{Expression={$_.type};Label="type";width=8}, @{Expression={$_.source};Label="source";width=40}, @{Expression={$_.query};Label="query";width=60}, @{Expression={$_.tables};Label="tables";width=60}
-
 # create $parser
 $sqldom = [System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.TransactSql.ScriptDom");
 if (-not $sqldom ) { ErrorExit; } #$sqldom.gettypes()
-$parser = new-object Microsoft.SqlServer.TransactSql.ScriptDom.TSql110Parser($false);
+
+if ([bool] ($sqldom.gettypes() | where-object {$_.name -eq $version } )) {
+	$parser = new-object Microsoft.SqlServer.TransactSql.ScriptDom.$version($false);
+} else {
+	write-host ("[ERR ] can't create '$version'. can't continue.") -foregroundcolor red;
+	exit;
+}
 
 # string or file or directory
 $sources = selectr($source)
@@ -335,7 +261,7 @@ $sources = $buffertable
 $buffertable = $null
 
 if ($er) {
-	write-host ("[WARN] parsed error. can't continue, exit.") -foregroundcolor red;
+	write-host ("[ERR ] detect parse error. can't continue.") -foregroundcolor red;
 	exit;
 }
 
@@ -401,10 +327,10 @@ $buffertable | % {
 		$crud.add($stmts.source, @{C = @();R = @();U = @();D = @(); });
 	}
 	switch ($stmts.stmt.gettype().name) {
-		"CreateTableStatement" { $stmts.tables | % { $crud[$stmts.source]["C"] += $_; } }
+		"CreateTableStatement" { $stmts.stmt; $stmts.tables | % { $crud[$stmts.source]["C"] += $_; } }
 		"SelectStatement" { $stmts.tables | % { $crud[$stmts.source]["R"] += $_; } }
 		"InsertStatement" { $stmts.tables | % { $crud[$stmts.source]["C"] += $_; } }
-		"IfStatement" { $stmts.stmt  }
+		"IfStatement" { $stmts.stmt.ThenStatement.StatementList.Statements.QueryExpression  }
 		"UpdateStatement" { $stmts.tables | % { $crud[$stmts.source]["U"] += $_; } }
 		"DeleteStatement" { $stmts.tables | % { $crud[$stmts.source]["D"] += $_; } }
 		"DropTableStatement" { $stmts.tables | % { $crud[$stmts.source]["D"] += $_; } }
@@ -420,3 +346,15 @@ foreach ($key in $crud.Keys) {
 	" "
 }
 
+<#
+R   	select
+RC  	select into
+D   	delete from
+DR  	delete from ()
+U   	update
+UR  	update from ()
+C   	insert into
+CR  	insert into select from
+C   	insert into exec proc
+
+#>
